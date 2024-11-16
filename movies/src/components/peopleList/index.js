cf8327cf94ee116e51f87212c406f56464475278
Zalán Tóth <zalant.tsc@gmail.com>
2024-11-16 22:31:18 +0000

    Added linking to person details

--- movies/src/components/peopleList/index.js
+++ movies/src/components/peopleList/index.js
@@ -3,6 +3,7 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
+import {Link} from "react-router-dom";
 
 const PeopleList = (props) => {
     //<Person key={m.id} person={m} action={props.action} />
@@ -12,12 +13,12 @@ const PeopleList = (props) => {
             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
         >
             <TableCell component="th" scope="row">
-                {p.name}
+                <Link to={`/people/${p.id}`}>{p.name}</Link>
             </TableCell>
             <TableCell align="right">{p.known_for_department}</TableCell>
             <TableCell align="right">{p.popularity}</TableCell>
-            <TableCell align="right">{p.gender}</TableCell>
-            <TableCell align="right">{p.adult}</TableCell>
+            <TableCell align="right">{p.gender === 1 ? "Female" : "Male"}</TableCell>
+            <TableCell align="right">{p.adult ? "Minor" : "Adult"}</TableCell>
         </TableRow>
     ));
     return peopleCards;
